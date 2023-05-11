ccb276a2bade8c8d2bd961cae7e4c351cb202f1d
João Manoel <jm.lima_@outlook.com>
2023-05-11 11:51:03 -0300

    primeiro commit

---
+++ nome.rb
@@ -0,0 +1,8 @@
+print "digite seu nome:"
+nome = gets.chomp
+print "digite seu sobrenome:"
+sobrenome = gets.chomp
+print "digite sua idade:"
+idade = gets.chomp
+
+puts "olá #{nome} #{sobrenome} ! seja bem vindo(a)! sua idade é #{idade}."
